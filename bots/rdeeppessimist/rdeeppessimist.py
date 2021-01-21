c121e7c1234c8bc7f4fdb91cd36e4c71bc7aab34
Michael Cochez <michaelcochez@>
2021-01-21 23:26:34 +0100

    Debugged the bot. It appears to work as expected

--- bots/rdeeppessimist/rdeeppessimist.py
+++ bots/rdeeppessimist/rdeeppessimist.py
@@ -8,7 +8,7 @@ RdeepBot - This bot looks ahead by following a random path down the game tree. T
 # Import the API objects
 from api import State, Deck, util
 import random
-
+from typing import List, Tuple
 
 class Bot:
 
@@ -17,7 +17,7 @@ class Bot:
 	# How deep to sample
 	__depth = -1
 
-	def __init__(self, num_samples=4, depth=8):
+	def __init__(self, num_samples=1, depth=8):
 		self.__num_samples = num_samples
 		self.__depth = depth
 
@@ -53,6 +53,7 @@ class Bot:
 
 		return best_move # Return the best scoring move
 
+	@staticmethod
 	def make_pessimistic_assumption(state: State):
 		"""
 		This code was moved here from deck and state. Trying to iompletment make_assumption outside of these classes.
@@ -71,60 +72,64 @@ class Bot:
 	
 		#we need to make the updated deck, for this we need the representation of the state of the cards, the beliefs of the players about the cards, the content of the stock and the trump suit
 		
-		oldPerspective = state.get_perspective()
+		oldCurrentPlayerPerspective = state.get_perspective()
 		
 		## we find the current unknowns.
-		unknowns = [index for index, card in enumerate(oldPerspective) if card == "U"]
+		unknowns = [index for index, card in enumerate(oldCurrentPlayerPerspective) if card == "U"]
 		#unknowns = the total number of cards we do not know. Both in the hand of the other player and on the stock
 		
 		#the other player has 5 cards in had, so this is the number of cards that player also does not know.
 		# Note that we might know some of these cards because of mariages.
-		other_player_unknowns = 5 - oldPerspective.count(other_player_term)
+		other_player_unknowns = 5 - oldCurrentPlayerPerspective.count(other_player_term)
 		numberOfUnknownForStock = len(unknowns) - other_player_unknowns
 
 		#now, we split the cards for the hand of the other player and the stock
-		toOtherPlayerHand, toStock = splitcards(unknowns, other_player_unknowns, numberOfUnknownForStock)
+		toOtherPlayerHand, toStock = Bot.splitcards(unknowns, other_player_unknowns, numberOfUnknownForStock)
 
 		# we nnow create a new perspective, thereby also filling in the unknowns
-		newPerspective = list(oldPerspective)
+		newGlobalPerspective = list(oldCurrentPlayerPerspective)
 		for i in toOtherPlayerHand:
-			newPerspective[i] = other_player_term
+			newGlobalPerspective[i] = other_player_term
 
-		trump_index = oldPerspective.index("S")
+		trump_index = oldCurrentPlayerPerspective.index("S")
 		#We place the rest of the unknowns on  the stock. Keeping the trump on the bottom.
 		stock = [trump_index] + toStock
 		#Note, we did not touch the 'S'  representing the trump card in the newPerspective at all, so it is still set
-		assert newPerspective[trump_index] == 'S' #Better safe than sorry
+		assert newGlobalPerspective[trump_index] == 'S' #Better safe than sorry
 
 		for i in toStock:
-			newPerspective[i] = "S"
+			newGlobalPerspective[i] = "S"
 		#the whole state should be known by now		
-		assert not 'U' in newPerspective
+		assert not 'U' in newGlobalPerspective
 
 		# Now we get to a tricky part. we need to create a vector to indicate the knowledge of the other. This means, we have to create the perspective for the other player.
 		# This we will do based on our own perspective on the cards, the one of the current_player. 
 		# In the standard implementation of make_assumption the perspective of the other player is directly copied. However, see https://github.com/intelligent-systems-course/schnapsen/issues/15
 		# We do make one mistake in the implementation here. In what we do, the oponent players will loose information regarding past marriages and trump exchanges.
 		# in order to preservce this, we would need to either remeber these things happened, or have access to the opponent's perspective.
-		current_player_perspective = oldPerspective # the current player hasn't obtained more knowledge compared to what was known before
-		other_player_perspective = list(newPerspective) #we make a copy of the new state of the cards and will then blind out things
+		current_player_perspective = oldCurrentPlayerPerspective # the current player hasn't obtained more knowledge compared to what was known before
+		other_player_perspective = list(newGlobalPerspective) #we make a copy of the new state of the cards and will then blind out things
 		# We do create the perspective by making the following replacements in the newly created state:
 		# If a card is marked as being in the hand of the current player, the other player cannot know, so we make it unknown. Note, here the mistake is made that the opponent would not know any of our cards.
-		other_player_perspective = ['U' if cardStatus == current_player_term for cardStatus in other_player_perspective]
+		other_player_perspective = ['U' if cardStatus == current_player_term else cardStatus for cardStatus in other_player_perspective]
 		# If a card is marked as being on the stock, the other player cannot now, except for the trump card, which we set back
-		other_player_perspective = ['U' if cardStatus == 'S' for cardStatus in other_player_perspective]
+		other_player_perspective = ['U' if cardStatus == 'S' else cardStatus for cardStatus in other_player_perspective]
 		other_player_perspective[trump_index] = 'S'
 		
 		#The deck does not care about current and other, only about 1 and 2
-		if currentplayer == 1
+		if currentplayer == 1:
 			player1_perspective = current_player_perspective
 			player2_perspective = other_player_perspective
-		else ## currentplayer == 2
+		else: ## currentplayer == 2
 			player2_perspective = current_player_perspective
 			player1_perspective = other_player_perspective
 
 
-		deck = Deck(newPerspective, stock, player1_perspective , player2_perspective, trump_suit)
+		print (oldCurrentPlayerPerspective)
+		print (newGlobalPerspective)
+		print (player1_perspective)
+		print (player2_perspective)
+		deck = Deck(newGlobalPerspective, stock, player1_perspective , player2_perspective, trump_suit)
 
 
 		player1s_turn = currentplayer == 1
@@ -136,10 +141,12 @@ class Bot:
 
 		#we need to make a new state
 		newState = State(deck, player1s_turn, p1_points, p2_points, p1_pending_points, p2_pending_points)
-		
+		# print (state)
+		# print (newState)
 		return newState
 
-	def splitcards (cards: list[int], neededForOtherPlayer: int, neededForStock: int) -> tuple[list[int], list[int]]:
+	@staticmethod
+	def splitcards (cards: List[int], neededForOtherPlayer: int, neededForStock: int) -> Tuple[List[int], List[int]]:
 		assert len(cards) == neededForOtherPlayer + neededForStock
 		####### here you would change the order in unknowns #####
 		#For now just shuffling them with a fixed seed for testing
